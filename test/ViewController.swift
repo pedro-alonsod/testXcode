75fb0ed520ac8533f2653e061a4f71de1ca006e3
Nora Hilda De los Reyes <nora.delosreyes@hotmail.com>
2015-08-02 14:59:44 -0500

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Nora Hilda De los Reyes on 02/08/15.
+//  Copyright © 2015 pedro alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
